d0225460445ee46a9ae732457b5ebb9f10b01064
princeps091-binf <princeps091@gmail.com>
2022-12-26 14:46:11 +0100

    test against expected refference

--- echor_vipin/tests/cli.rs
+++ echor_vipin/tests/cli.rs
@@ -1,6 +1,6 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
-
+use std::fs;
 type TestResult = Result<(), Box<dyn std::error::Error>>;
 
 
@@ -17,4 +17,32 @@ Ok(())
 fn runs() {
 let mut cmd = Command::cargo_bin("echor_vipin").unwrap();
 cmd.arg("hello -n").assert().success();
+}
+
+#[test]
+fn hello1() {
+let outfile = "tests/expected/hello1.txt";
+let expected = fs::read_to_string(outfile).expect("inadequate string file");
+let mut cmd = Command::cargo_bin("echor_vipin")
+.unwrap();
+println!("{:?}",expected);
+cmd.arg("Hello there")
+.arg("-n")
+.assert()
+.success()
+.stdout(expected);
+}
+
+#[test]
+fn hello2() {
+let outfile = "tests/expected/hello2.txt";
+let expected = fs::read_to_string(outfile).expect("inadequate string file");
+let mut cmd = Command::cargo_bin("echor_vipin")
+.unwrap();
+cmd.arg("Hello")
+.arg("there")
+.arg("-n")
+.assert()
+.success()
+.stdout(expected);
 }
