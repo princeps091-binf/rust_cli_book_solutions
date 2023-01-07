14d8d331f685fac241f7d1f3969fcc622ef550aa
princeps091-binf <princeps091@gmail.com>
2023-01-07 19:42:44 +0100

    finalised tool

--- catr/target/debug/catr.d
+++ catr/target/debug/catr.d
@@ -1,1 +1,1 @@
-/home/vipink/Documents/Rust_projects/rust_cli_book_solutions/catr/target/debug/catr: /home/vipink/Documents/Rust_projects/rust_cli_book_solutions/catr/src/main.rs
+/home/vipink/Documents/Rust_projects/rust_cli_book_solutions/catr/target/debug/catr: /home/vipink/Documents/Rust_projects/rust_cli_book_solutions/catr/src/lib.rs /home/vipink/Documents/Rust_projects/rust_cli_book_solutions/catr/src/main.rs
---
+++ catr/target/debug/libcatr.d
@@ -0,0 +1,1 @@
+/home/vipink/Documents/Rust_projects/rust_cli_book_solutions/catr/target/debug/libcatr.rlib: /home/vipink/Documents/Rust_projects/rust_cli_book_solutions/catr/src/lib.rs
