a2e4daf970f2de52057ae8d498e61dfba11b5b38
princeps091-binf <princeps091@gmail.com>
2023-01-09 21:36:49 +0100

    commad line skeleton

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,8 +1,53 @@
 use std::error::Error;
+use clap::{Arg, command, ArgAction};
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
+
+
+pub fn get_args() -> MyResult<Config> {
+
+    let matches = command!()
+    .arg(
+        Arg::new("files")
+            .short('f')
+            .long("files")
+            .help("Files to concatenate")
+            .action(ArgAction::Append)
+        )
+        .arg(
+            Arg::new("number")
+                .short('n')
+                .long("number")
+                .help("Number lines")
+                .action(ArgAction::SetTrue)
+        )
+        .arg(
+            Arg::new("number_nonblank")
+                .short('b')
+                .long("number-nonblank")
+                .help("Number nonblank lines")
+                .action(ArgAction::SetTrue)
+        )
+    .get_matches();
+
+    Ok(Config {
+        files: matches.get_many("files")
+        .unwrap()
+        .map(|v: &String| v.to_string())
+        .collect(),
+        number_lines: *matches.get_one("number").unwrap(),
+        number_nonblank_lines: *matches.get_one("number_nonblank").unwrap(),
+        })
+}
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-pub fn run() -> MyResult<()>{
-    println!("Hello, Wolrd!");
+pub fn run(config: Config) -> MyResult<()>{
+    dbg!(config);
     Ok(())
 }
--- catr/target/debug/deps/catr-146a30522be776a0.d
+++ catr/target/debug/deps/catr-146a30522be776a0.d
@@ -3,3 +3,8 @@
 /home/vipink/Documents/Rust_projects/rust_cli_book_solutions/catr/target/debug/deps/catr-146a30522be776a0.d: src/lib.rs
 
 src/lib.rs:
+
+# env-dep:CARGO_PKG_AUTHORS=Vipin Kumar <princeps091@gmail.com>
+# env-dep:CARGO_PKG_DESCRIPTION=My Rust implementation of cat
+# env-dep:CARGO_PKG_NAME=catr
+# env-dep:CARGO_PKG_VERSION=0.1.0
