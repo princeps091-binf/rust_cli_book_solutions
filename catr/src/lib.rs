584e4def197f9221e1eaf1bdea330d03f6484f4f
princeps091-binf <princeps091@gmail.com>
2023-01-10 21:41:30 +0100

    multiple files allowed

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -18,31 +18,32 @@ pub fn get_args() -> MyResult<Config> {
             .long("files")
             .help("Files to concatenate")
             .action(ArgAction::Append)
+            .num_args(0..)
             .default_value("_")
         )
-        .arg(
-            Arg::new("number")
-                .short('n')
-                .long("number")
-                .help("Number lines")
-                .action(ArgAction::SetTrue)
-                .conflicts_with("number_nonblank")
+    .arg(
+        Arg::new("number")
+            .short('n')
+            .long("number")
+            .help("Number lines")
+            .action(ArgAction::SetTrue)
+            .conflicts_with("number_nonblank")
         )
-        .arg(
-            Arg::new("number_nonblank")
-                .short('b')
-                .long("number-nonblank")
-                .help("Number nonblank lines")
-                .action(ArgAction::SetTrue)
-                .conflicts_with("number")
-        )
+    .arg(
+        Arg::new("number_nonblank")
+            .short('b')
+            .long("number-nonblank")
+            .help("Number nonblank lines")
+            .action(ArgAction::SetTrue)
+            .conflicts_with("number")
+    )
     .get_matches();
 
     Ok(Config {
-        files: matches.get_many("files")
-        .unwrap()
-        .map(|v: &String| v.to_string())
-        .collect(),
+        files: matches.get_many::<String>("files")
+        .unwrap_or_default()
+        .map(|v| v.to_string())
+        .collect::<Vec<String>>(),
         number_lines: *matches.get_one("number").unwrap(),
         number_nonblank_lines: *matches.get_one("number_nonblank").unwrap(),
         })
@@ -51,6 +52,8 @@ pub fn get_args() -> MyResult<Config> {
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 pub fn run(config: Config) -> MyResult<()>{
-    dbg!(config);
+    for filename in config.files {
+        println!("{}",filename);
+    }
     Ok(())
 }
